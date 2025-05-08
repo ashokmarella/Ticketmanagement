b10d957e825180315c91a282e4480e2b87a83e64
ashokmarella <129413921+ashokmarella@users.noreply.github.com>
2025-05-08 15:46:42 +0530

    add control component and change statly individually

--- src/app/shared/button/button.component.ts
+++ src/app/shared/button/button.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'button[appbutton]',
+  selector: 'button[appbutton],a[appButton]',
   standalone: true,
   imports: [],
   templateUrl: './button.component.html',
